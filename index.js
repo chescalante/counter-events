d5339c071d57171774af6a0d683f0ded8e2dc4a2
Christian Escalante <chescalante.ar@gmail.com>
2021-06-15 15:48:41 -0300

    added contract abi

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Web3 = require('web3')
+const counter = require('./counter-abi.json')
 
 const webSocketProvider = new Web3.providers.WebsocketProvider("127.0.0.1:8545")
 
 const web3 = new Web3(webSocketProvider)
 
-const contract = new web3.eth.Contract(OneShotScheduleData.abi, contractAddress)
+const contract = new web3.eth.Contract(counter.abi, contractAddress)
 
 contract.events.Counted(
     { fromBlock: 'latest' },
