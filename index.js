941f7f6f62484462b5c3c9d3763ace41d76313dc
Christian Escalante <chescalante.ar@gmail.com>
2021-06-15 16:01:25 -0300

    minor changes

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 const Web3 = require('web3')
 const counter = require('./counter-abi.json')
 
-const contractAddress = ""
-const rpcUrl = "127.0.0.1:8545"
+let contractAddress = ""
+const rpcWsUrl = "ws://127.0.0.1:8545"
+const rpcHttpUrl = "http://127.0.0.1:8545"
 
-const webSocketProvider = new Web3.providers.WebsocketProvider(rpcUrl)
 
-const web3 = new Web3(webSocketProvider)
+const deployContract = async (
+    web3,
+    abi,
+    bytecode,
+    args
+  ) => {
+    const contract = new web3.eth.Contract(abi)
+    const deployer = contract.deploy({ data: bytecode, arguments: args })
+  
+    const from = web3.eth.defaultAccount
+  
+    const gas = await deployer.estimateGas({ from })
+  
+    return new Promise((resolve, reject) =>
+      deployer
+        .send({ from, gas })
+        .on('error', (error) => reject(error))
+        .then((newContractInstance) => resolve(newContractInstance))
+    )
+  }
 
-const contract = new web3.eth.Contract(counter.abi, contractAddress)
+const init = async () => {
+    const httpWeb3 = new Web3(rpcHttpUrl)
+    const accounts = await httpWeb3.eth.getAccounts()
 
-contract.events.Counted(
-    { fromBlock: 'latest' },
-    async (error, event) => {
-        console.log(error, event)
-    }
-)
+    httpWeb3.eth.defaultAccount = accounts[0]
+    
+    const deployedContract = await deployContract(httpWeb3, counter.abi, counter.bytecode)
+    contractAddress = deployedContract.options.address
+    
+    console.log("contractAddress", contractAddress)
+    
+    const webSocketProvider = new Web3.providers.WebsocketProvider(rpcWsUrl)
+    
+    const web3 = new Web3(webSocketProvider)
+    web3.eth.defaultAccount = accounts[0]
+
+    const contract = new web3.eth.Contract(counter.abi, contractAddress)
+
+    contract.events.Counted(
+        { fromBlock: 'latest' },
+        async (error, event) => {
+            console.log(error, event)
+        }
+    )
+
+    const gas = await contract.methods
+        .inc()
+        .estimateGas()
+
+    await contract.methods
+        .inc()
+        .send({ from: web3.eth.defaultAccount, gas: gas })
+}
+
+init()
+
+
