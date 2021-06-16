05a884d9a637b083a03aaeb4e1a113377951204a
Diego Grasso <diegograsso@iovlab.org>
2021-06-16 16:51:47 -0300

    ehters

---
+++ index-eth.js
@@ -0,0 +1,19 @@
+const ethers = require('ethers')
+const counter = require('./counter-abi.json')
+
+let contractAddress = "0xfB602d3E9F3941cCd6792447d12221d54F6C51A0"
+const rpcWsUrl = "ws://127.0.0.1:4445"
+
+const init = () => {
+    console.log("contractAddress", contractAddress)
+    const webSocketProvider = new ethers.providers.WebSocketProvider(  rpcWsUrl ) 
+    const contract = new web3.eth.Contract(counter.abi, contractAddress)
+
+    contract.on("Counted", function(event){ console.log(event)})
+
+    console.log("Ready")
+  }
+
+init()
+
+
