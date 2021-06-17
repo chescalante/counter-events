3a50ac66bcb3f037d6ab144dffdebb17d3b664ac
Diego Grasso <diegograsso@iovlab.org>
2021-06-17 12:28:51 -0300

    Add slash

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Web3 = require('web3')
 const counter = require('./counter-abi.json')
 
 let contractAddress = "0xFB602D3e9F3941cCD6792447D12221d54F6c51A0"
-const rpcWsUrl = "ws://localhost:4445/websocket"
+const rpcWsUrl = "ws://localhost:4445/websocket/"
 
 const init = () => {
     console.log("contractAddress", contractAddress.toLowerCase())
