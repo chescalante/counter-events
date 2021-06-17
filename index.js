1a7cc95617cb47651a01c14c783250a844437e21
Christian Escalante <chescalante.ar@gmail.com>
2021-06-17 14:44:11 -0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let contractAddress = "0xFB602D3e9F3941cCD6792447D12221d54F6c51A0"
 const rpcWsUrl = "ws://localhost:4445/websocket/"
 const rpcHttpUrl = "http://127.0.0.1:4444"
 
-const init = () => {
+const init = async () => {
     console.log("contractAddress", contractAddress.toLowerCase())
     const web3Http = new Web3(rpcHttpUrl)
     const contractHttp = new web3Http.eth.Contract(counter.abi, contractAddress.toLowerCase())
