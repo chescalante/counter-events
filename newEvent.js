7059b6393d594666fd25468d3a1ec168f63510f2
Diego Grasso <diegograsso@iovlab.org>
2021-06-17 12:09:33 -0300

    pk

--- newEvent.js
+++ newEvent.js
@@ -4,18 +4,12 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 let contractAddress = "0xFB602D3e9F3941cCD6792447D12221d54F6c51A0"
 const rpcHttpUrl = "http://127.0.0.1:4444"
-const mnemonic = "skill upon stick change tissue immense oyster wide crush dinner slight theory"
+const privateKeys = ["01ccf471b564abe3f9c77f8b1745d57885212a2e5d2d3478e50665e913abd8d5"]
 
 const init = async () => {
     console.log("Sending...")
 
-    const hdWalletProvider = new HDWalletProvider({
-      mnemonic: mnemonic,
-      providerOrUrl: rpcHttpUrl,
-      numberOfAddresses: 1,
-      shareNonce: true,
-      derivationPath: "m/44'/60'/0'/0"
-    })
+    const hdWalletProvider = new HDWalletProvider(privateKeys, rpcHttpUrl, 0, 1);
 
     const web3 = new Web3(hdWalletProvider)
 
