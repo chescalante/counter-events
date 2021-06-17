c5aedf0f69649f5c025968e79fa0518bdb613516
Dana Luz <danaluz@MacBook-Air-de-Dana.local>
2021-06-17 14:33:47 -0300

    getpast events

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const counter = require('./counter-abi.json')
 
 let contractAddress = "0xFB602D3e9F3941cCD6792447D12221d54F6c51A0"
 const rpcWsUrl = "ws://localhost:4445/websocket/"
+const rpcHttpUrl = "http://127.0.0.1:4444"
 
 const init = () => {
     console.log("contractAddress", contractAddress.toLowerCase())
@@ -32,10 +33,23 @@ const init = () => {
 
     contract.events.allEvents({ fromBlock: 0 }, function(error, event){ console.log("x",event)})
 
-    setTimeout(()=>{
-      console.log("all")
-      contract.getPastEvents("Counted",{ fromBlock: 1932809, toBlock:'latest' }).then((r)=>{ console.log(r)})
-    },60000)
+    // with getPastEvents
+
+    const web3Http = new Web3(rpcHttpUrl)
+    const contractHttp = new web3Http.eth.Contract(counter.abi, contractAddress.toLowerCase())
+    let fromBlockNumber = await web3Http.eth.getBlockNumber() - 100
+
+    setInterval(()=>{
+      const toBlockNumber = await web3Http.eth.getBlockNumber()
+
+      console.log(`getting events from ${fromBlockNumber} to ${toBlockNumber}`)
+
+      contractHttp.getPastEvents("Counted",{ fromBlock: fromBlockNumber, toBlock: toBlockNumber }).then((r)=>{ 
+        console.log("getPastEvents", r)
+      })
+
+      fromBlockNumber = toBlockNumber
+    }, 60000)
 
     console.log("Ready")
   }
